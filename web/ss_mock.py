2c82eab723ba0097066486b230088d68c740cb7d
Will Charlton <willcharlton@exosite.com>
2015-10-17 13:00:48 -0500

    Updated web-backend.

--- web/ss_mock.py
+++ web/ss_mock.py
@@ -3,8 +3,11 @@
 import pyonep
 
 _1P = pyonep.onep.OnepV1()
+
 BELL_CIK = '213c297fa96efe50f56c2e62588e042f1c211a55'
 AUTH={'cik': BELL_CIK}
 
+
+
 while True:
-	print(_1P.wait(AUTH, 'soundscout',{'timeout': 5*60}))
+	print(_1P.wait(AUTH, {'alias': 'soundscout'},{}))
--- web/ssweb.py
+++ web/ssweb.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 
-import pyonep
+import pyonep, json
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
+
 _1P = pyonep.onep.OnepV1()
+
+AUTH_SSMaster = {'cik': '79dc7a6ef240a4d2de55504d3e2f2f5da3018619'}
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	info = _1P.info(AUTH_SSMaster, {'alias':''})
+	aliases = info[1]['aliases']
+	for rid in aliases:
+		r = _1P.read(AUTH_SSMaster, rid, {}, defer=True)
+	result = _1P.send_deferred(AUTH_SSMaster)
+	for r in result:
+		print(r)
+			
+	data_list = ''
+	for rid in aliases:
+		for r in result:
+			# print(r)
+			if r[0]['procedure'] == 'read' and\
+				r[0]['arguments'][0] == rid:
+				print("will", r[2])
+				data_list+='<li>{0} - {1}</li>'.format(aliases[rid], r[2])
+	page = """
+<html>
+	<body>
+	<h1>Soundscout Data</h1></br>
+	{0}
+	</body>
+</html>
+""".format(data_list)
+	return page
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
