d624539b0c82065459c39025c6fd9cff8b1e60bc
Will Charlton <willcharlton@exosite.com>
2015-10-17 13:54:30 -0500

    Working, stripped down version.

--- web/ssweb.py
+++ web/ssweb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import pyonep, json
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -10,16 +10,20 @@ _1P = pyonep.onep.OnepV1()
 
 AUTH_SSMaster = {'cik': '79dc7a6ef240a4d2de55504d3e2f2f5da3018619'}
 
+NUM_DATAPOINTS = 5
 @app.route("/")
-def hello():
+def main():
 	info = _1P.info(AUTH_SSMaster, {'alias':''})
 	aliases = info[1]['aliases']
+	print("aliases", aliases)
+
 	for rid in aliases:
-		r = _1P.read(AUTH_SSMaster, rid, {}, defer=True)
+		r = _1P.read(AUTH_SSMaster, rid, {'limit': NUM_DATAPOINTS}, defer=True)
 	result = _1P.send_deferred(AUTH_SSMaster)
 	for r in result:
 		print(r)
-			
+	
+	data_rows = {}
 	data_list = ''
 	for rid in aliases:
 		for r in result:
@@ -27,16 +31,9 @@ def hello():
 			if r[0]['procedure'] == 'read' and\
 				r[0]['arguments'][0] == rid:
 				print("will", r[2])
-				data_list+='<li>{0} - {1}</li>'.format(aliases[rid], r[2])
-	page = """
-<html>
-	<body>
-	<h1>Soundscout Data</h1></br>
-	{0}
-	</body>
-</html>
-""".format(data_list)
-	return page
+				data_rows[ aliases[rid][0] ] = [ r[2][d] for d in range(0, NUM_DATAPOINTS) ]
+				#data_list+='<li>{0} - {1}</li>'.format(aliases[rid], r[2])
+	return render_template('main.html', data_rows=data_rows)
 
 if __name__ == "__main__":
 	app.run()
