ff6a97b8fc1a22590b431a7e94bd9675b3bb573b
Daniel Feldman <dfeldman.mn@gmail.com>
2015-10-17 12:35:29 -0500

    Update to use pyonep, 10s window

--- is_an_airplane.py
+++ is_an_airplane.py
@@ -1,11 +1,12 @@
-from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
+#from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
 from scipy import fft, arange, ifft
 from numpy import sin, linspace, pi, zeros
 #from scipy.io.wavfile import read,write
 import scipy.io.wavfile, scipy.io, sys, glob, numpy 
 import time, recorder
 
-import urllib, urllib2, json
+import urllib, urllib3, urllib.parse, json
+import pyonep
 url = "http://m2.exosite.com/onep:v1/stack/alias"
 cik = "d9b359e0f00295d2e607471ac5797dde62528736"
 
@@ -17,19 +18,25 @@ amplitude_threshold = 1000
 airplane_ness_threshold = 0.00023
 
 def upload(amplitude, airplane_ness):
-   data = urllib.urlencode(
-      {'soundscout': json.dumps(
-         {'amplitude':amplitude,'airplane_ness':airplane_ness})})
-   headers={"X-Exosite-CIK":cik}
-   req = urllib2.Request(url, data, headers)
-   content = urllib2.urlopen(req).read()
-   print content
+#   data = {'soundscout': '666'}
+    data= json.dumps(
+         {'version':1,'amplitude':amplitude,'airplane_ness':airplane_ness})
+#   headers={"X-Exosite-CIK":cik,
+#            'Accept':'application/x-www-form-urlencoded; charset=utf-8' }
+#  http = urllib3.PoolManager()
+#  req = http.request_encode_body('POST', fields=data,url=url,headers=headers)
+#   print(req.fields, req.url, req.headers)
+#req = urllib3.Request(url, data, headers)
+#   content = req.read() #urllib2.urlopen(req).read()
+#   print(content, req.status)
+    o = pyonep.OnepV1()
+    o.write(cik, {'alias':'soundscout'}, data)
 
 def to_fft(y, Fs): 
    n = len(y) 
    Y = fft(y)/n # fft computing and normalization
    #print len(
-   Y = Y[range(freqrange)]
+   Y = Y[:freqrange] #range(freqrange)]
    return Y
 
 Fs = samplerate;  # sampling rate
@@ -38,7 +45,7 @@ def do_check():
    Ys = zeros(freqrange)
    rate,data=scipy.io.wavfile.read('tempfile.wav')
    if rate != 22500:
-      print "Rate REALLY NEEDS to be 22500"
+      #print "Rate REALLY NEEDS to be 22500"
       sys.exit(1)
    y=data[:samples]
    Y = to_fft(y, Fs)
@@ -60,15 +67,16 @@ def do_check():
    #print sum(norm), sum(Ys)
    airplane_ness = sum((Ys-norm)**2)
    amplitude = sum(abs(y))/samples
-   print sum(abs(y))/samples, airplane_ness
+   #print sum(abs(y))/samples, airplane_ness
+   print(amplitude, airplane_ness)
+   upload(amplitude,airplane_ness)
    if amplitude > amplitude_threshold and airplane_ness < airplane_ness_threshold:
-      upload(amplitude,airplane_ness)
-      print "PROBABLY AN AIRPLANE"
+      print("PROBABLY AN AIRPLANE")
    #plot(arange(24999), abs(Ys-norm)*1000)
    #show()
 
 while True:
-   print "RECORD"
+   #print "RECORD"
    recorder.record()
-   print "CHECK"
+   #print "CHECK"
    do_check()
