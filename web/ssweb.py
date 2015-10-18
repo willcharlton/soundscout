8ad203b73f95ba87e0c22a7fe7a54494251ff13d
Will Charlton <willcharlton@exosite.com>
2015-10-17 19:03:47 -0500

    Maps are working.

--- web/ssweb.py
+++ web/ssweb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import pyonep, json
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template
 from datetime import datetime
 
 app = Flask(__name__)
@@ -32,6 +32,27 @@ def main():
 				data_rows[ aliases[rid][0] ] = [ r[2][d] for d in range(0, NUM_DATAPOINTS) ]
 	return render_template('main.html', data_rows=data_rows)
 
+@app.route("/map")
+def map():
+	info = _1P.info(AUTH_SSMaster, {'alias':''})
+	aliases = info[1]['aliases']
+	print("aliases", aliases)
+
+	for rid in aliases:
+		r = _1P.read(AUTH_SSMaster, rid, {'limit': NUM_DATAPOINTS}, defer=True)
+	result = _1P.send_deferred(AUTH_SSMaster)
+	
+	data_rows = {}
+	data_list = ''
+	for rid in aliases:
+		for r in result:
+			if r[0]['procedure'] == 'read' and\
+				r[0]['arguments'][0] == rid:
+				print("will", r[2])
+				data_rows[ aliases[rid][0] ] = [ r[2][d] for d in range(0, NUM_DATAPOINTS) ]
+	return render_template('map.html', data_rows=data_rows)
+
+
 @app.template_filter('dtime')
 def dtime(ts):
 	return datetime.fromtimestamp( int(ts) ).strftime("%H:%M:%S-%Y/%m/%d")
@@ -39,9 +60,25 @@ def dtime(ts):
 @app.template_filter('unjson')
 def unjson(blob):
 	print(blob)
-	return """
-Loudness:      {0} db SPL
-Airplane-ness: {1} %
-""".format(json.loads(blob)['amplitude'], int(json.loads(blob)['airplane_ness']*100000)  )
+	return Markup("""
+<table>
+<tr><td>Loudness</td><td>{0}</td></tr>
+<tr><td>Airplane-ness</td><td>{1}</td></tr>
+</table>
+""".format(json.loads(blob)['amplitude'], 1000000*json.loads(blob)['airplane_ness']  ))
+
+@app.template_filter('gps')
+def gps(blob):
+	location = '44.98014269999999,-93.28874739999999'
+	if not json.loads(blob).get('location') == None:
+		location = json.loads(blob).get('location')
+	elif not json.loads(blob).get('address') == None:
+		location = json.loads(blob).get('address')
+	return [ float(i) for i in location.split(',') ]
+
 if __name__ == "__main__":
 	app.run(host = '0.0.0.0' if not app.debug else '127.0.0.1')
+
+
+
+
